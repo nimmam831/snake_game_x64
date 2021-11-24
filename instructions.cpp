b323477be1167bf575e4444a6a93f9f0f4c9e766
nimmam831 <87071422+nimmam831@users.noreply.github.com>
2021-11-24 08:53:44 -0500

    Create instructions.cpp

---
+++ instructions.cpp
@@ -0,0 +1,13 @@
+//How to play:
+//F is the fruit,
+//O is the head,
+//o's are the tail
+//To get a tail, touch the F.
+//It will spawn, and after it does, and you touch more, it gets longer.
+//#'s are the walls,
+//If you touch the wall, dont worry! You will not die, but go through the other side.
+//If you touch the tail, dont worry! You will not die, but your score will go lower and the tail gets shorter.
+//W/w: up,
+//A/a: left,
+//S/s: down,
+//D/d: right
