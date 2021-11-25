63363f75fec45d82a6c7198dd839d9a34daf2484
nimmam831 <87071422+nimmam831@users.noreply.github.com>
2021-11-24 20:20:08 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@
 #include <Windows.h>
 using namespace std;
 bool gameOver;
-const int width = 20;
-const int height = 20;
+const int width = 45;
+const int height = 25;
 int x, y, fruitX, fruitY, score;
 int tailX[100], tailY[100];
 int nTail;
