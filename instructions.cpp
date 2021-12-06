47d75e593e7d790136d14c45fcfd088dda430f5f
nimmam831 <87071422+nimmam831@users.noreply.github.com>
2021-12-06 15:20:53 -0500

    Fix typo.

--- instructions.cpp
+++ instructions.cpp
@@ -5,7 +5,7 @@
 //To get a tail, touch the F.
 //It will spawn, and after it does, and you touch more, it gets longer.
 //#'s are the walls,
-//If you touch the wall, dont worry! You will not die, but go through the other side.
+//If you touch the wall, dont worry! You will not die, but you will go through the wall.
 //If you touch the tail, dont worry! You will not die, but your score will go lower and the tail gets shorter.
 //W/w: up,
 //A/a: left,
